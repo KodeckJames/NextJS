af2ceb4a29b6fa72a32f25bea5559dbaee37e053
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-04-02 14:15:06 +0300

    catch all segments route

---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Reviews() {
+    return (
+      <>
+      <h1>Reviews</h1>
+            <p>Review 1</p>
+            <p>Review 2</p>
+            <p>Review 3</p>
+            <p>Review 4</p>
+            <p>Review 5</p>
+            <p>Review 6</p>
+            <p>Review 7</p>
+            <p>Review 8</p>
+            <p>Review 9</p>
+            <p>Review 10</p>
+        </>
+  )
+}
