8467197d513889b3a941fd372a7d355f654a6b75
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-04-27 22:41:38 +0300

    not-found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,12 @@
+import { notFound } from 'next/navigation';
+
 import React from 'react'
 
 export default async function ProductReview({ params }: { params: Promise<{ productId: string; reviewId: string }> }) {
-    const { reviewId, productId } = await params;
+  const { reviewId, productId } = await params;
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
   return (
       <div>Review {reviewId} for product {productId}</div>
   )
