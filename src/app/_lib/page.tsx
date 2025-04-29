87391ed7ab25def65e5f855abab72b21b665389a
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-04-29 12:01:28 +0300

    Private folder

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Lib() {
+  return (
+    <div>you cannot view this in the browser, but you can, when you prefix the folder name with %5F to mean an underscore </div>
+  )
+}
