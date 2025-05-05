2feba1cdf7d55511850efd43a5291f9ba68a76c7
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-05-05 17:47:57 +0300

    Links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function ProductList() {
+  const productID = 100;
   return (
-      <>
-          <h1>Product List</h1>
-          <h2>Product 1</h2>
-          <h2>Product 2</h2>
-          <h2>Product 3</h2>
-      </>
+    <>
+      <Link href="/">Home</Link>
+      <h1>Product List</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>
+        <Link href={`/products/${productID}`} replace>Product {productID}</Link>
+      </h2>
+    </>
   )
 }
