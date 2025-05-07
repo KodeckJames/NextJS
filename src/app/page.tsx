1a73b7e0586ac9b1b3adab465d31cbbe0dbf7ab0
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-05-07 13:48:31 +0300

    params and searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ import React from 'react'
 export default function Home() {
   return (
     <>
+      <div className=" flex flex-col">
       <h1>Welcome Home!</h1>
       <Link href="/blog">Blog</Link>
-      <Link href="/products" className=" block">products</Link>
+      <Link href="/products">products</Link>
+      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+        <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+        </div>
     </>
   )
 }
