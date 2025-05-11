ba8fb1c81d36d695c2a6433c38842866a3c4d49c
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-05-12 00:51:06 +0300

    Error handling and recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -2,12 +2,20 @@ import { notFound } from 'next/navigation';
 
 import React from 'react'
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random()*count)
+}
+
 export default async function ProductReview({ params }: { params: Promise<{ productId: string; reviewId: string }> }) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review")
+  }
   const { reviewId, productId } = await params;
   if (parseInt(reviewId) > 1000) {
     notFound();
   }
   return (
-      <div>Review {reviewId} for product {productId}</div>
+      <h1>Review {reviewId} for product {productId}</h1>
   )
 }
