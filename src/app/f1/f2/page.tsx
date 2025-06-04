5f213ee7f0aa96de74d16f35e853c46f5d910aff
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-04 08:55:39 +0300

    Intercepting routes

---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function F2() {
+  return (
+      <div>
+          F2 Page
+          <Link href={'/f4'}>F4 page</Link>
+    </div>
+  )
+}
