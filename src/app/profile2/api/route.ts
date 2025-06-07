716f12ebcdc466819023f4f2c43ad69450b4c8f9
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-08 01:00:31 +0300

    Headers in Route Handlers

--- src/app/profile2/api/route.ts
+++ src/app/profile2/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-    return new Response("User data");
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headerList = await headers();
+    console.log(headerList.get("Authoriaztion"));
+    
+    return new Response("<h1>Profile API data</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        }
+    });
 }
