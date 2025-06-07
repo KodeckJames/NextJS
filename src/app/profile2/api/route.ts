a9774f9f9b2989600e37f281997bc2a7b0cd1341
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-08 01:18:40 +0300

    Cookies in Route Handlers

--- src/app/profile2/api/route.ts
+++ src/app/profile2/api/route.ts
@@ -1,16 +1,23 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
-    // const requestHeaders = new Headers(request.headers);
-    // console.log(requestHeaders.get("Authorization"));
+    const requestHeaders = new Headers(request.headers);
+    console.log(requestHeaders.get("Authorization"));
 
     const headerList = await headers();
     console.log(headerList.get("Authoriaztion"));
+
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+    
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage", "20");
     
     return new Response("<h1>Profile API data</h1>", {
         headers: {
             "Content-Type": "text/html",
+            "set-cookie": "theme=dark"
         }
     });
 }
