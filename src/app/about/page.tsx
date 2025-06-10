e649879320c20a388083f84d9fae64180db206a0
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-10 13:00:53 +0300

    Static rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 export const metadata = {
   title: "About me",
 }
 
 function page() {
   return (
-    <div>About me</div>
+    <div>About me {new Date().toLocaleTimeString() }</div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       
       <div className=" flex flex-col">
       <h1>Welcome Home!</h1>
-      <Link href="/blog">Blog</Link>
-      <Link href="/products">products</Link>
+      <Link href="/dashboardRSC">dashboardRSC</Link>
+      <Link href="/about">about</Link>
       <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
         <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
         <Link href={'/profile'}><button className=" bg-blue-700 rounded-lg p-2 cursor-pointer" >Profile</button></Link>
