3c7a5eaa22abc69a59e09d49ba46154d50cf0cf9
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-12 00:01:10 +0300

    Third party packages

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,30 +1,30 @@
-// "use client";
+"use client";
 
-// import React from "react";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// export default function ServerRoutePage() {
-//   const settings = {
-//     dots: true,
-//   };
-//   return (
-//     <div className="image-slider-container">
-//       <Slider {...settings}>
-//         <div>
-//           <img src="https://picsum.photos/400/200" />
-//         </div>
-//         <div>
-//           <img src="https://picsum.photos/400/200" />
-//         </div>
-//         <div>
-//           <img src="https://picsum.photos/400/200" />
-//         </div>
-//         <div>
-//           <img src="https://picsum.photos/400/200" />
-//         </div>
-//       </Slider>
-//     </div>
-//   );
-// }
+export const ImageSlider=()=> {
+  const settings = {
+    dots: true,
+  };
+  return (
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+      </Slider>
+    </div>
+  );
+}
