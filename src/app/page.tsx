0772ed25cdfac88917ca969cab0b8c2c46d7e218
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-17 08:23:07 +0300

    Form Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-import Link from "next/link";
+import Link from 'next/link'
+import { Search } from '@/components/search'
 
 export default function Home() {
   return (
     <>
-      
       <div className=" flex flex-col">
-      <h1>Welcome Home!</h1>
-      <Link href="/dashboardRSC">dashboardRSC</Link>
-      <Link href="/about">about</Link>
-      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+        <h1>Welcome Home!</h1>
+        <Search />
+        <Link href="/dashboardRSC">dashboardRSC</Link>
+        <Link href="/about">about</Link>
+        <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
         <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
-        <Link href={'/profile'}><button className=" bg-blue-700 rounded-lg p-2 cursor-pointer" >Profile</button></Link>
+        <Link href={'/profile'}>
+          <button className=" bg-blue-700 rounded-lg p-2 cursor-pointer">
+            Profile
+          </button>
+        </Link>
         <Link href={'/dashboardRSC'}>Dashboard</Link>
-        </div>
+      </div>
     </>
   )
 }
