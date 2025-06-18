42142df4622d7865e5eafc5ea6a347c694dbd3d3
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-18 09:59:33 +0300

    Role based access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isProtectedRoute = createRouteMatcher(["/user-profile"]);
+
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect();
@@ -10,6 +14,13 @@ export default clerkMiddleware(async (auth, req) => {
     if (!userId && !isPublicRoute(req)) {
         return redirectToSignIn();
         //Here you can add custom logic to run before redirecting e.g. logging unauthorized access attempts for security monitoring, routing users to region-specific sign in pages, or handle maintenance windows for specific routes
+    }
+    if (
+        isAdminRoute(req) &&
+        (await auth()).sessionClaims?.metadata?.role !== "admin"
+    ) {
+        const url = new URL("/", req.url);
+        return NextResponse.redirect(url);
     }
 });
 
